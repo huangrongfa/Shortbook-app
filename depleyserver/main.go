e9194e9508132643f7e56c09c20f1b93df01d022
huangrongfa <760085259.com>
2019-06-01 18:52:18 +0800

    fix

--- depleyserver/main.go
+++ depleyserver/main.go
@@ -5,19 +5,22 @@ import (
 	"net/http" 
 	"os/exec" 
 	"log"
+	"fmt"
 )
 //
 func reLaunch () {
-	cmd := exec.Command("sh", "./depley.sh")
+	cmd := exec.Command("sh", "../depley.sh")
 	err := cmd.Start()
 	if err!= nil {
 		log.Fatal(err)
+		fmt.Println(err.Error())
 	}
 	err = cmd.Wait()
+	fmt.Println(err.Error())
 }
 //
 func propxy(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>欢迎学习go语言,并实现自动化部署,欢迎大家进入GO的学习殿堂</h1>")
+	io.WriteString(w, "<h1>实现自动化部署,欢迎大家进入GO的学习殿堂</h1>")
 	reLaunch()
 }
 //
