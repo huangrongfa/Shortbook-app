50bc7679e0230a1b6543617427f6ef24cf237bc6
huangrongfa <760085259.com>
2019-06-01 17:49:49 +0800

    实现自动化部署

--- depleyserver/main.go
+++ depleyserver/main.go
@@ -6,7 +6,7 @@ import (
 	"os/exec" 
 	"log"
 )
-
+//
 func reLaunch () {
 	cmd := exec.Command("sh", "./depley.sh")
 	err := cmd.Start()
@@ -15,12 +15,12 @@ func reLaunch () {
 	}
 	err = cmd.Wait()
 }
-
+//
 func propxy(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1>欢迎学习go语言,并实现自动化部署</h1>")
 	reLaunch()
 }
-
+//
 func main()  {
 	http.HandleFunc("/", propxy)
 	http.ListenAndServe(":5000", nil)
